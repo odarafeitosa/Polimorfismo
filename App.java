57554221d9ba828f1020d8340879441766f082f0
odarafeitosa <99831517+odarafeitosa@users.noreply.github.com>
2022-04-29 20:02:56 -0300

    Adicionado um "else if" na linha 25.

--- App.java
+++ App.java
@@ -5,12 +5,12 @@ public class App {
        Funcionario funcionario1 = new Funcionario("Eduardo", "88526545", "28/06/2000", "10/08/2019", 1700);
        Funcionario funcionario2 = new Funcionario("Maria", "88534545", "28/06/2003", "25/08/2019", 1700);
        ChefeDeDepartamento chefe = new ChefeDeDepartamento(funcionario1, "Departamento de TI", "10/04/2020");
-       PacienteClinica paciente = new PacienteClinica("José", "3454", "01/03/1990", "Sua Saúde");
+       PacienteClinica paciente = new PacienteClinica("JosÃ©", "3454", "01/03/1990", "Sua SaÃºde");
 
-       System.out.println(funcionario1.toString()+"\nEmpréstimo: "+ calculaEmprestimo(funcionario1));
-       System.out.println(funcionario2.toString()+"\nEmpréstimo: "+ calculaEmprestimo(funcionario2));
-       System.out.println(chefe.toString()+"\nEmpréstimo: "+ calculaEmprestimo(chefe));
-       System.out.println(paciente.toString()+"\nEmpréstimo: "+ calculaEmprestimo(paciente));
+       System.out.println(funcionario1.toString()+"\nEmprÃ©stimo: "+ calculaEmprestimo(funcionario1));
+       System.out.println(funcionario2.toString()+"\nEmprÃ©stimo: "+ calculaEmprestimo(funcionario2));
+       System.out.println(chefe.toString()+"\nEmprÃ©stimo: "+ calculaEmprestimo(chefe));
+       System.out.println(paciente.toString()+"\nEmprÃ©stimo: "+ calculaEmprestimo(paciente));
     }
     
     public static double calculaEmprestimo(Pessoa p) {
@@ -22,7 +22,7 @@ public class App {
     	if (f instanceof ChefeDeDepartamento) {
     		emprestimo = 4*(f.qualSalario());
     	}
-    	if (f instanceof Funcionario) {
+    	else if (f instanceof Funcionario) {
     		emprestimo = 2*(f.qualSalario());
     	}
     	
